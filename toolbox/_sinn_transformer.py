8b4521ef399002184040bad8fc38cb6f29010af9
Zihan-Xu527 <zxu64@ucmerced.edu>
2024-06-20 10:52:12 -0700

    boxplots

--- toolbox/_sinn_transformer.py
+++ toolbox/_sinn_transformer.py
@@ -18,7 +18,46 @@ class PositionalEncoding(nn.Module):
         self.register_buffer('pe', pe)
 
     def forward(self, x):
-        return x + self.pe[:x.size(0), :]   
+        return x + self.pe[:x.size(0), :] 
+    
+#time Absolute Position Encoding (tAPE)
+class tAPE(nn.Module): # Equation 13 page 11
+    def __init__(self, d_model, dropout=0.1, max_len=1024, scale_factor=1.0):
+        super(tAPE, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+        pe = torch.zeros(max_len, d_model)  # positional encoding
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin((position * div_term)*(d_model/max_len))
+        pe[:, 1::2] = torch.cos((position * div_term)*(d_model/max_len))
+        pe = scale_factor * pe.unsqueeze(0)
+        self.register_buffer('pe', pe)  # this stores the variable in the state_dict (used for non-trainable variables)
+    def forward(self, x):
+        x = x + self.pe
+        return self.dropout(x)
+
+    
+# class RelativePosition(nn.Module):
+
+#     def __init__(self, num_units, max_relative_position):
+#         super().__init__()
+#         self.num_units = num_units
+#         self.max_relative_position = max_relative_position
+#         self.embeddings_table = nn.Parameter(torch.Tensor(max_relative_position * 2 + 1, num_units))
+#         nn.init.xavier_uniform_(self.embeddings_table)
+
+#     def forward(self, length_q, length_k):
+#         range_vec_q = torch.arange(length_q)
+#         range_vec_k = torch.arange(length_k)
+#         distance_mat = range_vec_k[None, :] - range_vec_q[:, None]
+#         distance_mat_clipped = torch.clamp(distance_mat, -self.max_relative_position, self.max_relative_position)
+#         final_mat = distance_mat_clipped + self.max_relative_position
+#         final_mat = torch.LongTensor(final_mat).cuda()
+#         embeddings = self.embeddings_table[final_mat].cuda()
+
+#         return embeddings
+
+
 
 # class PositionalEncoding(nn.Module):
 #     def __init__(self, input_size, hidden_size, num_layers, bidirectional=False):
@@ -47,6 +86,7 @@ class SINN_transformer(nn.Module):
         self.embedding = nn.Linear(input_dimension, d_model)
 #         self.pos_encoder = PositionalEncoding(input_dimension,d_model,num_layers=1)
         self.pos_encoder = PositionalEncoding(d_model)
+#         self.pos_encoder = tAPE(d_model)
         encoder_layers = nn.TransformerEncoderLayer(d_model, nhead)
         self.transformer_encoder = nn.TransformerEncoder(encoder_layers, encoder_num_layers)
         
